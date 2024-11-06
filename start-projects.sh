a24bced6ba063de974dd897f9d98751b88de9f75
vadim <karjakin@visionservice.tech>
2024-11-06 21:59:50 +0300

    release 4.0.1

--- start-projects.sh
+++ start-projects.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /workspaces/tinkoff/backend
+cd /workspaces/tinkoff_futures/backend
 nohup nest start > backend.log 2>&1 &
 
-cd /workspaces/tinkoff/frontend
+cd /workspaces/tinkoff_futures/frontend
 nohup ng serve --host 0.0.0.0 --port 4200 > frontend.log 2>&1 &
