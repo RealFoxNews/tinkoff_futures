1a77af9d8071541f9f41df2eef5ba3581f756758
Vadim Karyakin <karjakin@visionsystems.tech>
2025-08-16 20:24:47 +0300

    feat: use bearer token for private port

---
+++ frontend/src/app/services/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = environment.authToken;
+    if (token) {
+      const separator = req.url.includes('?') ? '&' : '?';
+      const authReq = req.clone({
+        url: `${req.url}${separator}token=${token}`,
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(req);
+  }
+}
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://185.46.11.212/api/',
+  socketUrl: 'ws://185.46.11.212/api/',
+  authToken: '',
 };
